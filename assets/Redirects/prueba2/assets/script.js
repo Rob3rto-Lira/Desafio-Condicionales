597f3f0f5143e9057c93ab3b2f8f2d94c296bbeb
Rob3rto-Lira <rliraleon@gmail.com>
2025-01-12 13:59:55 -0300

    Second one done,

---
+++ assets/Redirects/prueba2/assets/script.js
@@ -0,0 +1,15 @@
+sticker1 = document.getElementById('stickers1');
+sticker2 = document.getElementById('stickers2');
+sticker3 = document.getElementById('stickers3');
+confirm = document.getElementById('confirm');
+respuesta = document.getElementById('respuesta');
+
+confirm.addEventListener('click', result);
+
+function result() {
+    if (Number(sticker1.value) + Number(sticker2.value) + Number(sticker3.value) > 10) {
+        respuesta.innerHTML = 'Elige menos de 10!';
+    } else {
+        respuesta.innerHTML = `llevas ${Number(sticker1.value) + Number(sticker2.value) + Number(sticker3.value)} stickers`
+    }
+}
